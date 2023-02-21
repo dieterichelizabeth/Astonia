78d1dec9dd1eaa720bedb760c32a980922a432cf
Elizabeth Dieterich <dieterichelizabeth@gmail.com>
2023-02-21 15:15:40 -0600

    v1 fin

--- README.md
+++ README.md
@@ -1,31 +1,189 @@
-# Astonia 🌱
-
 A plugin that creates a custom "Faculty" Post Type for displaying College Faculty and Staff using a form. Uses Divi Themes blog module and Post Types Order plugin!
-
-## Screenshots
-
-<img width="1163" alt="Screenshot 2023-02-16 at 11 36 02 PM" src="https://user-images.githubusercontent.com/95142863/219560010-d21d1cdb-b494-4189-943a-b513524b82aa.png">
 
 ## How it works
 
-This Plugin creates a custom post type for Wordpress and gathers data based on custom meta fields. These fields appear as a form labled "Faculty Information" when a user edits, or creates a new Faculty post. The data from the Meta fields form input are written to a template when the user "saves" or "updates" a post. The Divi Theme displays these post templates in a Grid form to make "cards".
+This Plugin creates a custom post type for Wordpress and gathers data based on custom meta fields. These fields appear as a form labled "Faculty Information" when a user edits, or creates a new Faculty post. The data from the Meta fields form input are written to a HTML template (saved as a string and updated to the post content) when the user "saves" or "updates" a post. The Divi Theme displays these post templates in a Grid form to make "cards".
 
-## Installation 🛠️
+# Local Wordpress Installation
 
-For local development see `notes/installation.md`
+## Steps to Setup the Plugin/Other tools 🛠️
 
-## Technologies used
+1. Add the `Fullerton Faculty Cards` Plugin and Activate it
 
-<span><img src="https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white">
-<img src="https://img.shields.io/badge/Wordpress-21759B?style=for-the-badge&logo=wordpress&logoColor=white">
-<img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
-<img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"></span>
+2. From the Dashboard, add the following CSS code to Appearance --> Customize --> "Additional CSS"
 
-## Reading List 📚
+```
+/* Styles to Faculty Cards */
+.fc-container {
+  width: 100%;
+  margin-bottom: 30px;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  float: left;
+  position: relative;
+  z-index: 2;
+  min-height: 1px;
+  vertical-align: baseline;
+  background: transparent;
+  color: #666;
+  line-height: 1.7em;
+  font-weight: 500;
+  border: 1px solid #919090c3;
+  padding-top: 0px;
+}
 
-- https://developer.wordpress.org/reference/functions/wp_update_post/
-- https://gist.github.com/fgilio/38dfbf9d03654b21302a
-- https://developer.wordpress.org/block-editor/reference-guides/block-api/block-patterns/
-- https://wpmudev.com/blog/creating-content-custom-post-types/
-- https://tommcfarlin.com/update-post-in-save-post-action/
-- https://wordpress.stackexchange.com/questions/51363/how-to-avoid-infinite-loop-in-save-post-callback
+.fc-header {
+  margin-top: 0px;
+  margin-bottom: 30px;
+  text-align: left;
+  border-color: #9b9b9b #9b9b9b #f1f1f1 #9b9b9b;
+  padding-bottom: 0px !important;
+  position: relative;
+  border: 0 solid #333;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  vertical-align: baseline;
+  background: transparent;
+}
+
+.fc-header-title {
+  font-size: 18px;
+  font-family: "Arial", Helvetica, Arial, Lucida, sans-serif;
+  color: #0f406b;
+  line-height: 1em;
+  background-color: #f1f1f1;
+  border-color: #9b9b9b #9b9b9b #f1f1f1 #9b9b9b;
+  padding-top: 8px !important;
+  margin-block-start: 0px;
+  margin-block-end: 0px;
+  padding-left: 10px !important;
+  position: relative;
+  border: 0 solid #333;
+  letter-spacing: 0.4px;
+}
+
+.fc-ht-el {
+  margin-block-start: 0px !important;
+  margin-block-end: 0px !important;
+  font-weight: 100 !important ;
+}
+
+p {
+  margin-block-start: 0px !important;
+  margin-block-end: 0px !important;
+}
+
+.fc-content {
+  margin-bottom: 0;
+  line-height: 1em;
+  border-width: 1px;
+  border-color: #f1f1f1 #9b9b9b #9b9b9b #9b9b9b;
+  height: 300px;
+  padding-top: 10px !important;
+  margin-top: -30px !important;
+  box-shadow: -7px 10px 22px 2px rgb(0 0 0 / 30%);
+  word-wrap: break-word;
+  position: relative;
+  border: 0 solid #333;
+  background-size: cover;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  color: #666;
+  font-weight: 500;
+  text-align: center;
+}
+
+.fc-img {
+  border: 1px solid #eaeaea;
+  border-radius: 8px;
+  max-height: 120px;
+}
+
+.fc-program {
+  font-family: "Arial", Helvetica, Arial, Lucida, sans-serif;
+  font-size: 14px;
+  line-height: 1em;
+  font-weight: 500;
+}
+
+.fc-text {
+  font-size: 14px;
+  font-weight: 100 !important;
+}
+
+.fc-link {
+  color: #0f406b;
+  text-decoration: none;
+  font-weight: 600;
+  font-size: 14px;
+  letter-spacing: 0.4px;
+  font-family: "Arial", Helvetica, Arial, Lucida, sans-serif;
+}
+
+.faculty .entry-title {
+  display: none;
+}
+```
+
+3. Add the `Post Types Order` Plugin, activate, and configure:
+
+- From the Dashboard, go to Settings Page make the configuration:
+  - "Hide" re-order interface for all posts other than "Faculty".
+  - "Show" re-order interface for the "Faculty" post type
+  - Minimum level to use this plugin - "Editor"
+  - Save Settings
+
+When it's done, this should look something like -
+
+![Screenshot 2023-02-21 at 3 07 48 PM](https://user-images.githubusercontent.com/95142863/220458851-cddc7afe-84e4-49a7-9aca-5af54bc2f7e5.png)
+
+4. Activate the `Divi theme` (if not already activated)
+
+## Steps to create your Faculty Cards 🛠️
+
+1. Click "Add new" on the Faculty plugin page
+
+2. Fill in the "Faculty Information" form fields
+
+- You can get the Photo URL by going to the "Media" tab --> clicking on the desired image --> then copy the File URL to the clipboard
+
+<p align="center">
+<img width="1175" alt="Screenshot 2023-02-17 at 12 50 32 PM" src="https://user-images.githubusercontent.com/95142863/219759075-ff982e5b-5b56-4185-a56e-de71966ebc66.png">
+</p>
+<p align="center">Photo URL circled in red</p>
+
+<p align="center"><img width="1065" alt="Screenshot 2023-02-16 at 11 24 20 PM" src="https://user-images.githubusercontent.com/95142863/219559415-a782e42a-8117-4563-8d5b-13c78332930b.png"></p>
+<p align="center">Example Form Filled out for a Faculty Member</p>
+
+- Note: Add the Title in `last name, first name` format to make sorting by Alphabetical order easier later on
+
+3. Publish the post
+
+   - You'll see post content populate after refreshing the page
+
+4. Create a few more faculty members by repeating steps 1 - 3
+
+5. Reorder the Faculty member Posts in Alphabetical order by drag and drop
+
+<p align="center"><img width="1092" alt="Screenshot 2023-02-16 at 11 52 05 PM" src="https://user-images.githubusercontent.com/95142863/219560711-cb2cf815-4ab5-4946-b2b2-72244bb77257.png"></p>
+
+<p align="center">Posts listed in Alphabetical order A-Z by last name</p>
+
+6. Create a New Page for the Faculty and Staff.
+
+7. In your new page, edit with the Divi Builder, and add the "Blog" Module and configure:
+
+- Content Tab:
+  - Content Dropdown: Post Type - Faculty, Content Length - Show Content
+  - Elements Dropdown: Set to "No" for all options
+- Design Tab:
+  - Layout Dropdown: Grid
+  - Border Dropdown: Set Grid Layout Border Width to 0px
+
+8. Save the Page
+
+- Note: the Faculty Post Titles will still appear in the Page editor, but will be hidden on the live url
+
+<p align="center"><img width="1163" alt="Screenshot 2023-02-16 at 11 36 02 PM" src="https://user-images.githubusercontent.com/95142863/219560010-d21d1cdb-b494-4189-943a-b513524b82aa.png"></p>
+
+<p align="center">Resulting Grid Cards</p>
--- notes/faculty-form-posty-type.php
+++ notes/faculty-form-posty-type.php
@@ -7,63 +7,67 @@
  * License: GPL2
  */
 
-/* Custom Post Type Start */
+// Slug: id? https://www.wpbeginner.com/glossary/post-slug/#:~:text=In%20WordPress%2C%20the%20%E2%80%9Cslug%E2%80%9D,post%E2%80%9D%20is%20the%20post%20slug.
 
-// Register The Faculty Post Type
-// show_in_rest: Whether to include the post type in the REST API - available in the block editor (grid).
+// Set up the arguments, and register The Faculty Post Type
 function create_faculty_post_type() {
+    // https://developer.wordpress.org/reference/functions/get_post_type_labels/
     $labels = array(
         'name' => __( 'Faculty' ),
         'singular_name' => __( 'Faculty' ),
         'menu_name' => __( 'Faculty' ),
         'add_new' => __( 'Add New' ),
-        'add_new_item' => __( 'Add New Faculty' ),
+        'add_new_item' => __( 'Add New Faculty' ), // Add new <Post Type Name>
         'edit' => __( 'Edit' ),
-        'edit_item' => __( 'Edit Faculty' ),
-        'new_item' => __( 'New Faculty' ),
-        'view' => __( 'View Faculty' ),
-        'view_item' => __( 'View Faculty' ),
-        'search_items' => __( 'Search Faculty' ),
-        'not_found' => __( 'No Faculty found' ),
+        'edit_item' => __( 'Edit Faculty' ), // Edit <Post Type Name>
+        'new_item' => __( 'New Faculty' ), // New <Post Type Name>
+        'view' => __( 'View Faculty' ), // ?
+        'view_item' => __( 'View Faculty' ), // View <Post Type Name>
+        'search_items' => __( 'Search Faculty' ), // Search <Post Type Name>
+        'not_found' => __( 'No Faculty found' ), // No <Post Type Name> found
         'not_found_in_trash' => __( 'No Faculty found in trash' ),
-        'parent' => __( 'Parent Faculty' ),
+        'parent' => __( 'Parent Faculty' ), // ?
     );
 
+    // Default arguments - https://developer.wordpress.org/reference/functions/register_post_type/
+    // Set public argument to true - https://justintadlock.com/archives/2013/09/13/register-post-type-cheat-sheet
     $args = array(
         'labels' => $labels,
-        'public' => true,
-        'has_archive' => true,
-        'menu_icon' => 'dashicons-groups',
-        'supports' => array( 'title', 'editor' ),
-        'show_in_rest' => true,
+        'public' => true, // Controls how the type is visible to authors (show_in_nav_menus, show_ui) ?
+        'has_archive' => true, // the post type name will be used for the archive slug
+        'menu_icon' => 'dashicons-groups', // Group of people menu icon
+        'supports' => array( 'title', 'editor' ), // Include a title and editor (content) for the post type. We'll add the custom-fields later
+        'show_in_rest' => true, // Inclue this post Type in the REST API (make available to the block editor)
     );
 
     register_post_type( 'faculty', $args );
 }
 
-// Hooking up our function to theme setup   
+// Register custom post types on the 'init' hook.  
 add_action( 'init', 'create_faculty_post_type' );
 
 // Add Meta Form fields
 function add_faculty_meta_boxes() {
-    // Create Meta Box
+    // Adds a meta box: https://developer.wordpress.org/reference/functions/add_meta_box/
     add_meta_box(
         'faculty_info', // Unique ID
-        'Faculty Information', // Box title
-        'display_faculty_meta_box', // Content callback
+        'Faculty Information', // Title of the meta box
+        'display_faculty_meta_box', // Content callback - fills the box with desired content
         'faculty', // Post type
-        'normal', // Position
-        'high' // Priority
+        'normal', // Context parameter - where the meta box is placed: https://wordpress.stackexchange.com/questions/2026/what-is-the-advanced-context-in-add-meta-box
+        'high' // Priority parameter - position the meta box: https://www.sitepoint.com/adding-custom-meta-boxes-to-wordpress/
     );
 }
 
+// Fires after all built-in meta boxes have been added: https://developer.wordpress.org/reference/hooks/add_meta_boxes/
 add_action( 'add_meta_boxes', 'add_faculty_meta_boxes' );
 
-// Output the meta fields (display to the User)
+// Output/Display Meta fields to the user
 function display_faculty_meta_box( $post ) {
 
-    // Retrieve the current values for these custom fields
-    // $post->ID: The ID of the post you want to retrieve the custom field value
+    // Get the current values for these custom fields
+    // Retrieves a post meta field for the given post ID: https://developer.wordpress.org/reference/functions/get_post_meta/
+    // Parameters: the Post ID, meta key to retrieve, return a single value (- do not return an array of values, just the value of the meta field)
     $name = get_post_meta( $post->ID, 'faculty_name', true );
     $photo_url = get_post_meta( $post->ID, 'faculty_photo_url', true );
     $email = get_post_meta( $post->ID, 'faculty_email', true );
@@ -72,10 +76,12 @@ function display_faculty_meta_box( $post ) {
     $office = get_post_meta( $post->ID, 'faculty_office', true);
     $linkedIn = get_post_meta( $post->ID, 'faculty_linkedIn_url', true);
 
-    // Output the fields in the meta box
+    // Output the fields in the meta box - the HTML form itself when you click "Add new" or "Edit"
+    // echo: outputs one or more strings like - echo "your text"; https://www.w3schools.com/php/func_string_echo.asp#:~:text=Definition%20and%20Usage,will%20generate%20a%20parse%20error.
+    // esc_attr(): All dynamic data must be escaped with esc_attr() before rendered in an html attribute.  may contain valid data that causes the html to break. A lone double quote would result in invalid markup which may cause the page to render poorly. https://developer.wordpress.com/themes/escaping/  https://wordpress.stackexchange.com/questions/298963/what-is-the-use-of-esc-attr-function
+    // < ? php: start php
+    // ? > end php
     ?>
-    <p>The Title of this Post should have the format - Last name, First Name. For example: Smith, John</p>
-    <p>Note: Do not make changes in the Editor above, it will be overwritted with data from this form.</p>
     <p>
         <label for="faculty_name">Faculty Name:</label>
         <input type="text" name="faculty_name" value="<?php echo esc_attr( $name ); ?>">
@@ -107,11 +113,15 @@ function display_faculty_meta_box( $post ) {
     <?php
 }
 
-// Update the Meta Form fields
+// Save the custom field data by key when the post is saved
 function save_faculty_meta_data( $post_id ) {
-    // Save the custom field data by key when the post is saved
+    // If this field was submitted, update the post id's meta field with user input
+
+    // isset( $_POST['faculty_name'] ) --> IF the form field with the attribute faculty_name was submitted
     if ( isset( $_POST['faculty_name'] ) ) {
+        // sanitize_text_field - Sanitizes a string from user input or from the database. https://developer.wordpress.org/reference/functions/sanitize_text_field/
         $name = sanitize_text_field( $_POST['faculty_name'] );
+        // Updates a post meta field based on the given post ID. Parameters - post_id, meta_key name, meta_value to be updated
         update_post_meta( $post_id, 'faculty_name', $name);
     }
 
@@ -146,14 +156,16 @@ function save_faculty_meta_data( $post_id ) {
     }
 }
 
+// Fires once a post has been saved, specify the post type slug: https://developer.wordpress.org/reference/hooks/save_post_post-post_type/
 add_action( 'save_post_faculty', 'save_faculty_meta_data' );
 
-// Update the Post's content
+// Save the updated Post's content conditionally
 function update_faculty_post_content( $post_id ) {
-    // Template Start
+    // Content Template Start
     $post_content = "<div class='fc-container'>";
 
-    // Conditionally update the template by user input
+    // If the field was submitted AND the input is not empty, append new HTML/data from the user input
+    // . =  concatenation operator in php (to update the template, we're concatenating the HTML string for the updated Post content)
     if ( isset( $_POST['faculty_name'] ) && (sanitize_text_field( $_POST['faculty_name'] ) !== "") ) {
         $post_content .= "<div class='fc-header'><div class='fc-header-title'><h4 class='fc-ht-el'>" . sanitize_text_field( $_POST['faculty_name'] ) . "</h4></div></div><div class='fc-content'>";
     }
@@ -182,19 +194,23 @@ function update_faculty_post_content( $post_id ) {
         $post_content .= "<p><a class='fc-link' href=" . sanitize_text_field( $_POST['faculty_linkedIn_url'] ) . ">LinkedIn Profile</a></p>";
     }
 
-    // Template End
+    // Content Template End
     $post_content .= "</div>";
     
     $post_data = array(
-        'ID' => $post_id,
-        'post_content' => $post_content
+        'ID' => $post_id, // The post's id
+        'post_content' => $post_content // The post content. Default empty. STRING https://developer.wordpress.org/reference/functions/wp_update_post/
     );
 
     // Stop that infinate loop :)
-    remove_action( 'save_post_faculty', 'update_faculty_post_content', 20 );
+    // 20: specify the priority https://wordpress.stackexchange.com/questions/51363/how-to-avoid-infinite-loop-in-save-post-callback
+    remove_action( 'save_post_faculty', 'update_faculty_post_content', 20 ); 
+
+    // Updates a post with new post data https://developer.wordpress.org/reference/functions/wp_update_post/
     wp_update_post( $post_data );
     add_action( 'save_post_faculty', 'update_faculty_post_content', 20 );
 
 }
 
-add_action( 'save_post_faculty', 'update_faculty_post_content', 20 );
+// Fires once a post has been saved, specify the post type slug: https://developer.wordpress.org/reference/hooks/save_post_post-post_type/
+add_action( 'save_post_faculty', 'update_faculty_post_content', 20 ); 
